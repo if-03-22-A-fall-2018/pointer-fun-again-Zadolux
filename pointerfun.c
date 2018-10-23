7aafb5ecfae0643da7d152c6900fd4414251ec27
zadolux <zadolux@inscomers.net>
2018-10-23 10:45:52 +0200

    Added block comment

---
+++ pointerfun.c
@@ -0,0 +1,18 @@
+/*----------------------------------------------------------
+ *				HTBLA-Leonding / Class: <your class>
+ * ---------------------------------------------------------
+ * Exercise Number: 6
+ * Title:     Pointer fun... again..
+ * Author:    Wallinger Marc
+ * ----------------------------------------------------------
+ * Description:
+ * Calculates a pyramid of numbers, i.e., it multiplies a big
+ * integer by the number 2, its result by 3, etc. and then
+ * returns to the original value by subsequently dividing,
+ * again starting by 2, 3, etc.
+ * ----------------------------------------------------------
+ */
+
+int main(int argc, char const *argv[]) {
+  return 0;
+}
