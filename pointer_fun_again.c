aff8788815330f14660b281afb01312a72631010
zadolux <zadolux@inscomers.net>
2018-10-23 11:21:27 +0200

    Added code in main

--- pointer_fun_again.c
+++ pointer_fun_again.c
@@ -13,6 +13,19 @@
  * ----------------------------------------------------------
  */
 
+struct PlayStruct {
+  int int_value;
+  double double_value;
+  char string_value[64];
+};
+
 int main(int argc, char const *argv[]) {
+  struct PlayStruct play_struct;
+  struct PlayStruct play_struct_pt;
+  struct PlayStruct* play_struct_pointer;
+  play_struct_pointer = &play_struct_pt;
+
+  play_struct_pointer->int_value = 3;
+  play_struct_pointer->double_value = 3.141;
   return 0;
 }
