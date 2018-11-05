f070fe0f30350c5dc92903aec874f90e862fbc9d
zadolux <zadolux@inscomers.net>
2018-11-05 17:27:04 +0100

    Removed description

--- pointer_fun_again.c
+++ pointer_fun_again.c
@@ -6,10 +6,7 @@
  * Author:    Wallinger Marc
  * ----------------------------------------------------------
  * Description:
- * Calculates a pyramid of numbers, i.e., it multiplies a big
- * integer by the number 2, its result by 3, etc. and then
- * returns to the original value by subsequently dividing,
- * again starting by 2, 3, etc.
+ *
  * ----------------------------------------------------------
  */
 #include <stdio.h>
